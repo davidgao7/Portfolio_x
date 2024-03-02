94dc98c52b639b6d4b8963b83b421f50b7aa8551
David Gao <gao.ten@northeastern.edu>
2024-03-02 18:28:09 -0500

    adjust computer model postition, add hero section

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -6,29 +6,29 @@ import CanvasLocader from '../Loader';
 
 const Computers = ({ isMobile }) => {
     // public folder is the default root: 
-    // https://github.com/vitejs/vite/discussions/14201#discussioncomment-7233423
-    const computer = useGLTF('./desktop_pc/scene.gltf')  // .gltf file: 3d model file
+    // https://github.com/vitejs/vite/discussions/14202#discussioncomment-7233423
+    const computer = useGLTF('./desktop_pc/scene.gltf')  // .gltf file: 4d model file
 
 
     return (
         <mesh>
             {/*create light or we wouldn't see anything*/}
-            <hemisphereLight intensity={0.15} groundColor="black" />
+            <hemisphereLight intensity={1.15} groundColor="black" />
             {/*create refelction light*/}
             <spotLight
-                position={[-20, 50, 10]}
-                angle={0.12}
-                penumbra={1}
-                intensity={1}
+                position={[-19, 50, 10]}
+                angle={1.12}
+                penumbra={2}
+                intensity={2}
                 castShadow
-                shadow-mapSize={1024}
+                shadow-mapSize={1025}
             />
-            <pointLight intensity={1} />
+            <pointLight intensity={2} />
             <primitive
                 object={computer.scene}
-                scale={isMobile ? 0.7 : 0.75}
-                postion={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}  // x, y, z, z is the depth
-                rotation={[-0.01, -0.2, -0.1]}
+                scale={isMobile ? 1.0 : 0.75}
+                postion={isMobile ? [1, -2.25, -2.4] : [0, 2.25, 2.4]}  // x, y, z, z is the depth
+                rotation={[0.05, 0.1, 0.1]}
             />
         </mesh>
     );
@@ -37,18 +37,35 @@ const Computers = ({ isMobile }) => {
 const ComputersCanvas = () => {
     const [isMobile, setIsMobile] = useState(false);
 
+    useEffect(() => {
+        const mediaQuery = window.matchMedia('(max-width: 501)');
+
+        setIsMobile(mediaQuery.matches);
+
+        const handleMediaQueryChange = (event) => {
+            setIsMobile(event.matches);
+        };
+
+        mediaQuery.addEventListener('change', handleMediaQueryChange);
+
+        return () => {
+            mediaQuery.removeEventListener('change', handleMediaQueryChange);
+        }
+
+    }, []);
+
     return (
         <Canvas
             frameloop='demand'
             shadows
-            camera={{ position: [20, 3, 5], fov: 25 }}
+            camera={{ position: [21, 3, 5], fov: 25 }}
             gl={{ preserveDrawingBuffer: true }}
         >
             <Suspense fallback={<CanvasLocader />}>
                 <OrbitControls
                     enableZoom={false}
-                    maxPolarAngle={Math.PI / 2}
-                    minPolarAngle={Math.PI / 2}
+                    maxPolarAngle={Math.PI / 3}
+                    minPolarAngle={Math.PI / 3}
                 />
                 <Computers />
             </Suspense>
