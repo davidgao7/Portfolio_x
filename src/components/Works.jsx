acc4870056dc9d6218afdb26f75b5e767f71636a
David Gao <gao.ten@northeastern.edu>
2024-03-08 10:31:43 -0500

    add dighub icon and project description

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -23,9 +23,38 @@ const ProjectCard = ({ index, name, description, tags, image, source_code_link }
                         alt='project_image'
                         className="w-full h-full object-cover rounded-2xl"
                     />
-                    <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
 
+                    <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
+                        {/*show github icon*/}
+                        <div
+                            onClick={() => window.open(source_code_link, "_blank")}
+                            className="black-gradient w-10 h-10 rounded-full flex justify-center
+                             items-center cursor-pointer"
+                        >
+                            <img
+                                src={github}
+                                alt="github_icon"
+                                className="w-1/2 h-1/2 object-contain"
+                            />
+                        </div>
                     </div>
+                </div>
+
+                <div className="mt-5">
+                    {/*display project discription*/}
+                    <h3 className="text-white font-bold text-[24px]">
+                        {name}
+                    </h3>
+                    <p className="mt-2 text-secondary text-[14px]"></p>
+                </div>
+
+                {/*show project tech stacks*/}
+                <div className="mt-4 flex flex-wrap gap-2">
+                    {tags.map((tag) => (
+                        <p key={`${name}-${tag.name}`} className={`text-[14px] $(tag.color)`}>
+                            #{tag.name}
+                        </p>
+                    ))}
                 </div>
             </Tilt>
         </motion.div>
@@ -36,8 +65,8 @@ const Works = () => {
     return (
         <>
             <motion.div variants={textVariant()}>
-                <p className={styles.sectionSubText}>My Work</p>
-                <h2 className={styles.sectionHeadText}>Projects</h2>
+                <p className={`${styles.sectionSubText}`}>My Work</p>
+                <h2 className={`${styles.sectionSubText}`}>Projects</h2>
             </motion.div>
 
             <div className='w-full flex'>
