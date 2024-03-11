cf6327b352127f69b004827133dd685148c68c5b
David Gao <gao.ten@northeastern.edu>
2024-03-11 09:34:41 -0400

    set the port and base path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
+// Alternatively, you can use the defineConfig helper which should provide intellisense 
+// without the need for jsdoc annotations
+// Vite also directly supports TS config files. You can use vite.config.ts with the 
+// defineConfig helper as well.
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    base: '/hello-world/', // absolute base path
+    mode: 'development',
+    server: {
+        port: 3000,
+    }
 })
